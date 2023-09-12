44fa4d173f9abcf37deede74ea13f283e6a31e5e
Sorin-Ioan-Alexandru Bîrchi <84462350+SorinAlexB@users.noreply.github.com>
2023-09-13 01:43:55 +0300

    DB configurations and templates

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,12 +1,42 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 from django.db import connection
+from . import views
+from django.urls import path
+from .models import Question 
+from django.template import loader  
+from django.shortcuts import render
+from .models import Question
+
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+# def index(request):
+#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
+#     template = loader.get_template("polls/index.html")
+#     context = {
+#         "latest_question_list": latest_question_list,
+#     }
+#     return HttpResponse(template.render(context, request))
 
-def queries(request):
-    mycursor = connection.cursor()
-    for x in mycursor:
-        HttpResponse(x)
+# Or use render to display the list
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
+# we use render if we just want a straight forward rendering without much control over it
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
