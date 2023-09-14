63c3c33d2aa400513a98d3f0fb435ba8227fbe5f
Sorin-Ioan-Alexandru Bîrchi <84462350+SorinAlexB@users.noreply.github.com>
2023-09-14 03:11:39 +0300

    Add files via upload

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 
 from .models import Question
 
-admin.site.register(Question)
+#This is the default admin
+# admin.site.register(Question) 
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
+
+
+admin.site.register(Question, QuestionAdmin)
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -8,8 +8,18 @@ from django.template import loader
 from django.shortcuts import render
 from .models import Question, Choice
 from django.views import generic
+from django.utils import timezone
 
 # Create your views here.
+def get_queryset(self):
+    """
+    Return the last five published questions (not including those set to be
+    published in the future).
+    """
+    return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[
+        :5
+    ]
+
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
@@ -22,12 +32,25 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
     model = Question
     template_name = "polls/results.html"
 
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
+    
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
